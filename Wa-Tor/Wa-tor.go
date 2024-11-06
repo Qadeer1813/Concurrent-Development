da48e64e4bd098e2f4a262995c215dff783b97e7
Qadeer1813 <c00270632@itcarlow.ie>
2024-11-06 15:16:13 +0000

    Initial Commit Wa-tor

---
+++ Wa-Tor/Wa-tor.go
@@ -0,0 +1,20 @@
+// --------------------------------------------
+// Author: Qadeer Hussain (C00270632@setu.ie)
+// Created on 04/11/2024
+// Modified by: Qadeer Hussain
+// Due: 29/11/2024
+// Wa Tor
+// --------------------------------------------
+
+package Wa_Tor
+
+func main() {
+	//	NumShark
+	//	NumFish
+	//	FishBreed
+	//	SharkBreed
+	//	Starve
+	//	GridSize
+	//	Threads
+
+}
