844af9ffadba8303ac494adf507e2b775c1899e0
Qadeer Hussain <113534010+Qadeer1813@users.noreply.github.com>
2024-11-25 12:38:42 +0000

    WIP

--- Wa-Tor/Wa-tor.go
+++ Wa-Tor/Wa-tor.go
@@ -11,6 +11,8 @@ package main
 
 import (
 	"fmt"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 	"math/rand"
 )
 
@@ -93,4 +95,9 @@ func main() {
 	initializeWorld()
 	printGrid()
 
+	a := app.New()
+	w := a.NewWindow("Hello World")
+
+	w.SetContent(widget.NewLabel("Hello World!"))
+	w.ShowAndRun()
 }
